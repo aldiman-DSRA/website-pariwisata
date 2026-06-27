0cf1285f6582e8fb4042c308178e7940c852f8e9
ALDIMAN <aldiman660@gmail.com>
2026-06-27 16:16:51 +0700

    user

--- application/views/website_public/layout/header.php
+++ application/views/website_public/layout/header.php
@@ -2,57 +2,43 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?></title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= $title ?></title>
 
-    <link rel="stylesheet" href="<?= base_url('assets1/style.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets1/style.css') ?>">
 </head>
 
 <body>
 
-<header>
-    <nav class="navbar">
+	<header>
+		<nav class="navbar">
 
-        <div class="logo">
-            <a href="<?= site_url('website') ?>">
-                <span>Nusantara</span> Travel
-            </a>
-        </div>
+			<div class="logo">
+				<a href="<?= site_url('website') ?>">
+					<span>Nusantara</span> Travel
+				</a>
+			</div>
 
-        <ul class="menu">
+			<ul class="menu">
 
-            <?php if(!$this->session->userdata('login')) : ?>
+				<li>
+					<a href="<?= site_url('website') ?>">Home</a>
+				</li>
 
-                <li>
-                    <a class="btn-login" href="<?= site_url('auth/login') ?>">
-                        Login
-                    </a>
-                </li>
+				<li>
+					<a href="<?= site_url('website/destinations') ?>">Destinations</a>
+				</li>
 
-            <?php else : ?>
+				<li>
+					<a href="<?= site_url('website/blog') ?>">Blog</a>
+				</li>
 
-                <li>
-                    <a href="<?= site_url('website/destinations') ?>">Destinations</a>
-                </li>
+				<li>
+					<a href="<?= site_url('website/about') ?>">About Us</a>
+				</li>
 
-                <li>
-                    <a href="<?= site_url('website/blog') ?>">Blog</a>
-                </li>
+			</ul>
 
-                <li>
-                    <a href="<?= site_url('website/about') ?>">About Us</a>
-                </li>
-
-                <li>
-                    <a class="btn-login" href="<?= site_url('auth/logout') ?>">
-                        Logout
-                    </a>
-                </li>
-
-            <?php endif; ?>
-
-        </ul>
-
-    </nav>
-</header>
+		</nav>
+	</header>
